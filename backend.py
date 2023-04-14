02dca1072a69f590aa75b62686e7dc61890626c2
Siitrev <paplata50@gmail.com>
2023-04-14 11:31:00 +0200

    commit

--- backend.py
+++ backend.py
@@ -1,17 +1,23 @@
-import openai
+import openai, environ, os
+
+env = environ.Env()
+env.read_env(".env")
+API_KEY = os.environ.get("API_KEY")
 
 
 class Chatbot:
     def __init__(self):
-        openai.api_key = "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"
-        
-        
+        openai.api_key = API_KEY
+
     def get_response(self, user_input):
-        response = openai.Completion.create(
-            engine = "text-davinci-003",
-            prompt=user_input,
-            max_tokens=4000,
-            temperature=0.5
-        ).choices[0].text
+        response = (
+            openai.Completion.create(
+                engine="text-davinci-003",
+                prompt=user_input,
+                max_tokens=4000,
+                temperature=0.5,
+            )
+            .choices[0]
+            .text
+        )
         return response
-        
